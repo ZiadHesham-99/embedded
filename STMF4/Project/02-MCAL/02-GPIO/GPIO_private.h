0ec1263f022fb53a16980a5a8259439a58e9b4c2
ZiadHesham-99 <76854651+ZiadHesham-99@users.noreply.github.com>
2022-08-17 18:16:23 +0200

    editted comments

--- STMF4/Project/02-MCAL/02-GPIO/GPIO_private.h
+++ STMF4/Project/02-MCAL/02-GPIO/GPIO_private.h
@@ -5,17 +5,6 @@
 #define GPIOB_BASE_ADDRESS  0x40020400
 #define GPIOC_BASE_ADDRESS  0x40020800
 
-/* GPPIOA registers */ 
-//#define GPIOA_MODER   *((uint32*)(GPIOA_BASE_ADDRESS+0x00))
-//#define GPIOA_OTYPER  *((uint32*)(GPIOA_BASE_ADDRESS+0x04))
-//#define GPIOA_OSPEEDR  *((uint32*)(GPIOA_BASE_ADDRESS+0x08))
-//#define GPIOA_PUPDR   *((uint32*)(GPIOA_BASE_ADDRESS+0x0C))
-//#define GPIOA_IDR     *((uint32*)(GPIOA_BASE_ADDRESS+0x10))
-//#define GPIOA_ODR     *((uint32*)(GPIOA_BASE_ADDRESS+0x14))
-//#define GPIOA_BSRR     *((uint32*)(GPIOA_BASE_ADDRESS+0x18))
-//#define GPIOA_LCKR     *((uint32*)(GPIOA_BASE_ADDRESS+0x1C))
-//#define GPIOA_AFRL     *((uint32*)(GPIOA_BASE_ADDRESS+0x20))
-//#define GPIOA_AFRH     *((uint32*)(GPIOA_BASE_ADDRESS+0x24))
 
 
 
